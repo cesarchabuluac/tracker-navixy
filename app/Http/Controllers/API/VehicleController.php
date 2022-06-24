df0eb18598c19b26dc437ffa9f08647dd0d7333d
Cesar chab <cesarchabuluac@gmail.com>
2022-06-24 14:05:52 -0500

    Fix tracker_id not null

--- app/Http/Controllers/API/VehicleController.php
+++ app/Http/Controllers/API/VehicleController.php
@@ -29,93 +29,101 @@ class VehicleController extends BaseController
     {
         $response = $service->listVehiclesNavixy();
         $response = json_decode($response, true);
-        if (boolval($response['success'])) {
-
-            DB::beginTransaction();
-
-            //Create Vehicles on database local
-            $data = [];
-            $trackersID = [];
-            collect($response['list'])->each(function ($item) use (&$data, &$trackersID) {
-                $trackersID[] = $item['tracker_id'];
-                $vehicle = Vehicle::find($item['id']);
-                if (empty($vehicle)) {
-                    $data[] = [
-                        'id' => $item['id'],
-                        'icon_color' => $item['icon_color'] ?? null,
-                        'tracker_id' => $item['tracker_id'] ?? null,
-                        'tracker_label' => $item['tracker_label'] ?? null,
-                        'label' => $item['label'] ?? null,
-                        'model' => $item['model'] ?? null,
-                        'type' => $item['type'] ?? null,
-                        'subtype' => $item['subtype'] ?? null,
-                        'color' => $item['color'] ?? null,
-                        'additional_info' => $item['additional_info'] ?? null,
-                        'reg_number' => $item['reg_number'] ?? null,
-                        'vin' => $item['vin'] ?? null,
-                        'created_at' => Carbon::now(),
-                        'updated_at' => Carbon::now(),
-                    ];
-                } else {
-                    $vehicle->icon_color = $item['icon_color'] ?? null;
-                    $vehicle->tracker_id = $item['tracker_id'] ?? null;
-                    $vehicle->tracker_label = $item['tracker_label'] ?? null;
-                    $vehicle->label = $item['label'] ?? null;
-                    $vehicle->model = $item['model'] ?? null;
-                    $vehicle->type = $item['type'] ?? null;
-                    $vehicle->subtype = $item['subtype'] ?? null;
-                    $vehicle->color = $item['color'] ?? null;
-                    $vehicle->additional_info = $item['additional_info'] ?? null;
-                    $vehicle->reg_number = $item['reg_number'] ?? null;
-                    $vehicle->vin = $item['vin'] ?? null;
-                    $vehicle->save();
-                }
-            });
-
-            if (!empty($data)) {
-                Vehicle::insert($data);
-            }
-
-            //Get tracking       
-            $response = $service->listTracking($trackersID);
-            $response = json_decode($response, true);
-
+        try {
             if (boolval($response['success'])) {
 
-                //Create Vehicles Tracking on database local
+                DB::beginTransaction();
+
+                //Create Vehicles on database local
                 $data = [];
-                $vehicleIds = [];
-                collect($response['states'])->each(function ($item, $index) use (&$data, &$vehicleIds) {
-                    $vehicle = Vehicle::where('tracker_id', $index)->first();
-                    if ($vehicle) {
-                        $vehicleIds[] = $vehicle->id;
+                $trackersID = [];
+                collect($response['list'])->each(function ($item) use (&$data, &$trackersID) {
+                    if (!empty($item['tracker_id'])) {
+                        $trackersID[] = $item['tracker_id'];
+                    }
+                    $vehicle = Vehicle::find($item['id']);
+                    if (empty($vehicle)) {
                         $data[] = [
-                            'vehicle_id' => $vehicle->id,
-                            'source_id' => $item['source_id'],
-                            'lat' => $item['gps']['location']['lat'],
-                            'lng' => $item['gps']['location']['lng'],
-                            'last_updated' => $item['last_update'],
-                            'movement_status' => $item['movement_status'],
+                            'id' => $item['id'],
+                            'icon_color' => $item['icon_color'] ?? null,
+                            'tracker_id' => $item['tracker_id'] ?? null,
+                            'tracker_label' => $item['tracker_label'] ?? null,
+                            'label' => $item['label'] ?? null,
+                            'model' => $item['model'] ?? null,
+                            'type' => $item['type'] ?? null,
+                            'subtype' => $item['subtype'] ?? null,
+                            'color' => $item['color'] ?? null,
+                            'additional_info' => $item['additional_info'] ?? null,
+                            'reg_number' => $item['reg_number'] ?? null,
+                            'vin' => $item['vin'] ?? null,
                             'created_at' => Carbon::now(),
                             'updated_at' => Carbon::now(),
                         ];
+                    } else {
+                        $vehicle->icon_color = $item['icon_color'] ?? null;
+                        $vehicle->tracker_id = $item['tracker_id'] ?? null;
+                        $vehicle->tracker_label = $item['tracker_label'] ?? null;
+                        $vehicle->label = $item['label'] ?? null;
+                        $vehicle->model = $item['model'] ?? null;
+                        $vehicle->type = $item['type'] ?? null;
+                        $vehicle->subtype = $item['subtype'] ?? null;
+                        $vehicle->color = $item['color'] ?? null;
+                        $vehicle->additional_info = $item['additional_info'] ?? null;
+                        $vehicle->reg_number = $item['reg_number'] ?? null;
+                        $vehicle->vin = $item['vin'] ?? null;
+                        $vehicle->updated_at = Carbon::now();
+                        $vehicle->save();
                     }
                 });
 
-                VehicleTracker::insert($data);
+                if (!empty($data)) {
+                    Vehicle::insert($data);
+                }
 
-                $vehicles = VehicleTracker::with('vehicle')->whereIn('vehicle_id', $vehicleIds)->latest('last_updated')->get()->unique('vehicle_id');
+                //Get tracking       
+                $response = $service->listTracking($trackersID);
+                $response = json_decode($response, true);
 
-                DB::commit();
+                if (boolval($response['success'])) {
 
-                return $vehicles;
+                    //Create Vehicles Tracking on database local
+                    $data = [];
+                    $vehicleIds = [];
+                    collect($response['states'])->each(function ($item, $index) use (&$data, &$vehicleIds) {
+                        $vehicle = Vehicle::where('tracker_id', $index)->first();
+                        if ($vehicle) {
+                            $vehicleIds[] = $vehicle->id;
+                            $data[] = [
+                                'vehicle_id' => $vehicle->id,
+                                'source_id' => $item['source_id'],
+                                'lat' => $item['gps']['location']['lat'],
+                                'lng' => $item['gps']['location']['lng'],
+                                'last_updated' => $item['last_update'],
+                                'movement_status' => $item['movement_status'],
+                                'created_at' => Carbon::now(),
+                                'updated_at' => Carbon::now(),
+                            ];
+                        }
+                    });
+
+                    VehicleTracker::insert($data);
+
+                    $vehicles = VehicleTracker::with('vehicle')->whereIn('vehicle_id', $vehicleIds)->latest('last_updated')->get()->unique('vehicle_id');
+
+                    DB::commit();
+
+                    return $vehicles;
+                } else {
+                    DB::rollBack();
+                    return $response;
+                }
             } else {
                 DB::rollBack();
                 return $response;
             }
-        } else {
+        } catch (Exception $ex) {
             DB::rollBack();
-            return $response;
+            return $this->sendError($ex->getMessage());
         }
     }
 
@@ -127,7 +135,6 @@ class VehicleController extends BaseController
     public function index(Request $request)
     {
         try {
-
 
             //Service Navixy
             $service = app()->make(TrackerNavixy::class);
